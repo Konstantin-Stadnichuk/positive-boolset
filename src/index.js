6d43b1d1b73837a113900c182a420f3971b37a2c
MINSK\Konstantin_Stadnichu <konstantin.stadnichuk@thomsonreuters.com>
2017-12-04 15:33:28 +0300

    Shit show

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function getPositiveKeysOfBoolset(boolset) {
-  // your solution here
+module.exports = function getPositiveKeysOfBoolset(array) {
+  var resultArr = [];
+  for (let i = 0; i < array.length; i += 2) {
+  	let value = array[i];
+  	let bool = array[i + 1];
+
+  	if (bool) resultArr.push(value);
+  }
+  return resultArr;
 }
